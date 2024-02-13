ee4b46a5bc60e005cecfa3b7187e0940cae8248d
Nexus <104232421+PHNexus@users.noreply.github.com>
2024-02-13 10:41:14 -0300

    Logica

--- main/script.js
+++ main/script.js
@@ -1,5 +1,18 @@
-let a = 12
-let b =  12
-let resultado = a + ``
 
-alert(`a soma desses numeros e,  ${resultado} `)
+// calculadora
+
+// ira pedir ao usuario um numero
+let a = parseFloat(prompt('digite um numero'))
+
+// ira pedir ao usuario um numero
+let b = parseFloat(prompt('digite outro numero'))
+
+// ira mostrar a soma dos numeros
+let resultado = (a + b)
+
+// typeof ira mostrar o tipo da variavel
+
+
+// ira abrir uma janela com o resultado
+alert(resultado)
+
