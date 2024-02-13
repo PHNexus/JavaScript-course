ddb4c8f3a5d10b9c19f7a2d75aa4e95d4ea8523b
Nexus <104232421+PHNexus@users.noreply.github.com>
2024-02-13 18:09:02 -0300

    add

---
+++ main/js.js
@@ -0,0 +1,18 @@
+var nome = ["Lucas", "Vinicius", "Edvaldo"];
+var novos =  nome.splice (1,2,"Leo", "Leonardo");
+console.log(nome);
+
+
+nome.push("Tiringa")
+
+console.log(nome)
+
+
+
+var a = nome.indexOf("Lucas")
+
+console.log(a)
+
+var n = nome.indexOf("Leo")
+
+console.log(n)
---
+++ main/js21.js
@@ -0,0 +1,3 @@
+var a = ["Lucas", "Paqueta", "Neymar", "Ronaldo"]
+var novos = a.slice(1,3)
+console.log(novos)
