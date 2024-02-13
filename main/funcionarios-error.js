2f9f0699893e8a16489d13fee9a0e52c0448ffb0
Nexus <104232421+PHNexus@users.noreply.github.com>
2024-02-13 19:42:12 -0300

    funcionario erro
    não aparece no terminal

---
+++ main/funcionarios-error.js
@@ -0,0 +1,14 @@
+const funcionarios = [
+    { nome: "helber", idade: 25 },
+    { nome: "marcos", idade: 15 },
+    { nome: "weslem", idade: 20 },
+    { nome: "tiringa", idade: 67 },
+];
+
+const pessoasListagem = funcionarios.filter((funcionario) => {
+    return funcionario.nome.length < 5;
+});
+
+console.log(pessoasListagem);
+
+
