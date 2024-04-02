d5fa9a2d84fc13cfec4d923014747a43e1bbcb2a
Nexus <104232421+PHNexus@users.noreply.github.com>
2024-04-01 22:40:11 -0300

    remove impropies lines

--- main/Logica-de-Programação/Pratica/pratica05.js
+++ main/Logica-de-Programação/Pratica/pratica05.js
@@ -1,7 +1,7 @@
 // JavaScript Basico
 
-var nome = prompt("qual seu nome fdp ")
-var idade = prompt("qual sua idade fdp ")
+var nome = prompt("qual seu nome ?")
+var idade = prompt("qual sua idade ?")
 var linguagem = prompt("Qual linguagem vc esta aprendendo")
 
 alert(`ola, ${nome}, pelo visto vc tem, ${idade} anos e esta aprendendo a linguagem ${linguagem}`)
