09d8675e1bb84eec30ac8e03b9569942c935a2fb
Nexus <104232421+PHNexus@users.noreply.github.com>
2024-05-31 09:02:26 -0300

    ta

--- main/Logica-de-Programação/Logica/Pratica/pratica06.js
+++ main/Logica-de-Programação/Logica/Pratica/pratica06.js
@@ -6,7 +6,9 @@ if (pais == 'Brazil'){ // se o pais for igual a brasil vc e brasileiro
 
 console.log('Brasileiro')
 
-}else( // se não voçẽ e estrangeiro
+}
+
+else( // se não voçẽ e estrangeiro
 
 console.log("Estrangeiro")
 
