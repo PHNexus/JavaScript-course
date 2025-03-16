0438f101d1e7553ac570a7caa520ef060a458032
Nexus <104232421+PHNexus@users.noreply.github.com>
2025-03-16 10:06:27 -0300

    indentando

--- main/arrays/fiter-map/funcionarios-error.js
+++ main/arrays/fiter-map/funcionarios-error.js
@@ -1,12 +1,12 @@
 const funcionarios = [
-    { nome: "helber", idade: 25 },
-    { nome: "marcos", idade: 15 },
-    { nome: "weslem", idade: 20 },
-    { nome: "tiringa", idade: 67 },
+  { nome: "helber", idade: 25 },
+  { nome: "marcos", idade: 15 },
+  { nome: "weslem", idade: 20 },
+  { nome: "tiringa", idade: 67 },
 ];
 
 const pessoasListagem = funcionarios.filter((funcionario) => {
-    return funcionario.nome.length > 5;
+  return funcionario.nome.length > 5;
 });
 
 console.log(pessoasListagem);
